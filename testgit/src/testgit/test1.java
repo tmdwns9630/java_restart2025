30f57eb91e3e383453243a8098713cda6303c0aa
tmdwn <tmdwn@LAPTOP-196AB45N>
2025-03-08 17:45:31 +0900

    2일차

--- testgit/src/testgit/test1.java
+++ testgit/src/testgit/test1.java
@@ -5,6 +5,9 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// git연동 테스트.
+		int value=30;
+		int result = value + 10;
+		System.out.println(result);
 	}
 
 }
